761eb2e02171bae14fda213041127487e3d290b2
ofirpro89 <ofirpro89@gmail.com>
2025-06-08 00:17:48 +0300

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import { defineConfig } from "vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
 export default defineConfig({
-  base: "/ofirpro89/",
+  base: "/ofirpro89/ReactProject",
   plugins: [react(), tailwindcss(), flowbiteReact()],
 });
