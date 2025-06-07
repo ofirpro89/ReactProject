2679fea43b11ba576316685b169f40c3f7f7558d
ofirpro89 <ofirpro89@gmail.com>
2025-06-08 01:03:21 +0300

    push

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,9 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import flowbiteReact from "flowbite-react/plugin/vite";
 
+// שים לב שהרישיות תואמות לשם הריפו ב-GitHub!
 export default defineConfig({
-  base: "/reactProject/",
+  base: "/ReactProject/",
   plugins: [react(), tailwindcss(), flowbiteReact()],
 });
+
